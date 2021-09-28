d1322f97466e6605931a947ec1393cb369d10146
wooseok <diddmlvkf@gmail.com>
2021-09-28 23:57:55 +0900

    CREATE: 브루트 포스/Four Squares

---
+++ 브루트 포스/Four Squares/ps1.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+start = int(N ** 0.5)
+dp = [0, 1]
+
+for i in range(2, N+1):
+    min_cnt = 4
+    j = 1
+
+    while (j ** 2) <= i:
+        min_cnt = min(min_cnt, dp[i - (j ** 2)])
+        j += 1
+    dp.append(min_cnt + 1)
+print(dp[N])
