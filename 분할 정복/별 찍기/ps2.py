4431b46c29ab5a0d4ce161e1189b5a9e25fa8727
wooseok <diddmlvkf@gmail.com>
2021-10-24 23:30:13 +0900

    ADD: 분할 정복/별 찍기 두번째 풀이

---
+++ 분할 정복/별 찍기/ps2.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+answer = [[" "] * (N*2) for _ in range(N)]
+
+def print_star(point, n_size):
+    global answer
+    r, c = point
+    if n_size == 3:
+        answer[r][c] = "*"
+        answer[r+1][c-1] = answer[r+1][c+1] = "*"
+        for i in range(3):
+            answer[r+2][c+i] = "*"
+            answer[r+2][c-i] = "*"
+    else:
+        n_size //= 2
+        print_star((r, c), n_size)
+        print_star((r+n_size, c-n_size), n_size)
+        print_star((r+n_size, c+n_size), n_size)
+
+print_star((0, N-1), N)
+
+for i in range(len(answer)):
+    for j in range(len(answer[i])):
+        print(answer[i][j], end="")
+    print()
