7e184ea2b33584552ecfe3b683d3f722bb919d17
tonic523 <diddmlvkf@gmail.com>
2022-01-17 18:49:01 +0900

    refactor(BOJ/N과 M (12)) : 두 번째 풀이 리팩토링

--- BOJ/N과 M/N과 M (12)/ps2.py
+++ BOJ/N과 M/N과 M (12)/ps2.py
@@ -3,19 +3,23 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().rstrip().split())
+numbers = list(set(map(int, input().rstrip().split())))
+numbers.sort()
+sequence = []
 
-li = list(set(map(int, input().rstrip().split())))
-li.sort()
-
-def bt(sequence):
-    if len(sequence) == M:
-        print(*sequence)
+# dfs 순회
+def dfs(count):
+    if count == M:
+        print(*map(int, sequence))
         return
 
-    for i in li:
-        if len(sequence) == 0 or sequence[-1] <= i:
-            sequence.append(i)
-            bt(sequence)
-            sequence.pop()
+    for n in numbers:
+        # 백 트래킹을 위한 조건
+        if sequence and sequence[-1] > n:
+            continue
 
-bt(list())
+        sequence.append(n)
+        dfs(count + 1)
+        sequence.pop()
+
+dfs(0)
