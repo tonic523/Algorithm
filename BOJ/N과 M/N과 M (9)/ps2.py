3767456b7c1acf77c47bec987bca4c98e7ee367d
tonic523 <diddmlvkf@gmail.com>
2022-01-17 18:36:21 +0900

    refactor(BOJ/N과 M (9)) : second solve

--- BOJ/N과 M/N과 M (9)/ps2.py
+++ BOJ/N과 M/N과 M (9)/ps2.py
@@ -3,22 +3,27 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().rstrip().split())
+numbers = list(map(int, input().rstrip().split()))
+numbers.sort()
+sequence, visited = [], [False] * len(numbers)
 
-numbers = sorted(list(map(int, input().rstrip().split())))
-answer, visited = [], [False] * N
-
-def dfs(size):
-    if size == M:
-        print(*answer)
+# dfs 순회
+def dfs(count):
+    if count == M:
+        print(*map(int, sequence))
         return
+
     t = -1
-    for i in range(N):
-        if visited[i] or t == numbers[i]:
+    for i in range(len(numbers)):
+        # 백 트래킹을 위한 조건(순열에 포함된 수, 중복된 수)
+        if visited[i] or numbers[i] == t:
             continue
-        visited[i] = True
-        answer.append(numbers[i])
-        dfs(size+1)
-        visited[i] = False
-        t = answer.pop()
+
+        sequence.append(numbers[i])  # 순열 추가
+        visited[i] = True  # 방문처리
+        dfs(count + 1)
+        visited[i] = False  # 방문처리 초기화
+        t = sequence.pop()  # 추가됐던 노드 저장
+
 
 dfs(0)
