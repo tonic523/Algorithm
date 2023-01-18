fa86d27f66659173d03a89cc0a61e6e543e5eccc
tonic523 <diddmlvkf@gmail.com>
2023-01-18 10:48:56 +0900

    feat: 프로그래머스/캐시 문제 풀이

---
+++ 프로그래머스/캐시/ps1.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque(maxlen=cacheSize)
+
+    if cacheSize == 0:
+        return len(cities) * 5
+
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            answer += 1
+        else:
+            answer += 5
+        cache.append(city)
+    return answer
